23e84520b2a84f1d1d249f6fed8c53dddbf80c12
Steven Fisher <geeksquad507@gmail.com>
2012-02-07 13:15:20 -0500

    Revert 4022579d908f70b0cf1af824aae7a58874edddb3^..HEAD

--- src/ConfigStuff.java
+++ src/ConfigStuff.java
@@ -3,6 +3,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ConfigStuff extends JavaPlugin {
 
+	public ConfigStuff(PreventBlocks instance){
+		
+	}
 	public void UpdateConfig(){
 		
 	}

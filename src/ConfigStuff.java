878db8d49d08c8663d26e4b9b81ad1eb99c40aa2
Steven Fisher <geeksquad507@gmail.com>
2012-02-07 13:28:30 -0500

    UpdateConfig adds warning node

--- src/ConfigStuff.java
+++ src/ConfigStuff.java
@@ -3,10 +3,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ConfigStuff extends JavaPlugin {
 
+	public PreventBlocks plugin;
 	public ConfigStuff(PreventBlocks instance){
-		
+		plugin = instance;
 	}
 	public void UpdateConfig(){
+		int warnBlocks = this.getConfig().getInt("Worlds.world.warn");
+		if(warnBlocks == 0){
+			this.getConfig().set("Worlds.world.warn", 46);
+		}
 		
 	}
 }

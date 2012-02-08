6906ffd171b949255450705c0195821e4566444c
Steven Fisher <geeksquad507@gmail.com>
2012-02-07 20:39:10 -0500

    Revert eb51118764a5f0be18445246675a3f34ace5d76f^..HEAD

--- src/ConfigStuff.java
+++ src/ConfigStuff.java
@@ -3,10 +3,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ConfigStuff extends JavaPlugin {
 
+<<<<<<< HEAD
 	public PreventBlocks plugin;
 	public ConfigStuff(PreventBlocks instance){
 		plugin = instance;
 	}
+=======
+>>>>>>> parent of 23e8452... Revert 4022579d908f70b0cf1af824aae7a58874edddb3^..HEAD
 	public void UpdateConfig(){
 		int warnBlocks = this.getConfig().getInt("Worlds.world.warn");
 		if(warnBlocks == 0){
